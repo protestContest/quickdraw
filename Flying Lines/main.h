ed88115ab99af02ce6a229744f51293b8c601f7e
Zack Michener <z@zjm.me>
2018-10-31 19:20:48 -0700

    add flying lines

---
+++ Flying Lines/main.h
@@ -0,0 +1,1 @@
+#define kMoveToFront		(WindowPtr)-1L#define kEmptyString		"\p"#define kEmptyTitle			kEmptyString#define kVisible			true#define kNoGoAway			false#define kNilRefCon			(long)nilshort gOldMBarHeight;void ToolBoxInit(void);void WindowInit(void);void setup(void);void draw(void);
